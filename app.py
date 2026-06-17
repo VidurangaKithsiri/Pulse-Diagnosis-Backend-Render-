f8fd400345faae99a4a1500d317961ebc5e7224f
surammyajith5@gmail.com <your-email@example.com>
2026-06-17 09:26:05 +0530

    Prepare Flask API for Render

--- app.py
+++ app.py
@@ -89,5 +89,8 @@ def predict():
         }), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=int(os.environ.get("PORT", 5000))
+    )
