4faa306321e3bc7d7ab6645ef8422a041767625c
surammyajith5@gmail.com <your-email@example.com>
2026-07-06 23:20:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,4 +140,8 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
+<<<<<<< HEAD
     )
+=======
+    )
+>>>>>>> 1fe53be (Update app.py)
